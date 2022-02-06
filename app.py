7a565a301024de01ec0f0389acbc054e88cab55f
ellouisefm <85605042+ellouisefm@users.noreply.github.com>
2022-02-07 07:41:38 +0800

    delete

--- app.py
+++
@@ -1,8 +0,0 @@
-import dash
-import dash_bootstrap_components as dbc
-
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
-server = app.server
-app.title = 'ROI Dashboard'
-# added
-server.secret_key = os.environ.get('secret_key', 'secret')
